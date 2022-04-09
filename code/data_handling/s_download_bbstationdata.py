9d1a61a35cbd15fe50eca788f7a9ba067575e372
bz <bhnzhang@gmail.com>
2022-04-08 23:24:40 -0400

    some reorganization

--- code/data_handling/s_download_bbstationdata.py
+++ code/data_handling/s_download_bbstationdata.py
@@ -13,20 +13,22 @@ import sched, time
 
 import os
 
-# main -------------------
+# ------------------------
+# function definitions
 
-# set refresh time in seconds
-refresh_t = 5*60 
-
-# set directory to save data
-# save_path = 'G:\\My Drive\\Personal projects\\bluebikes\\station data'
-save_path = 'D:\\Google Drive\\Personal projects\\bluebikes\\station data'
-
-# make scheduler object
-s = sched.scheduler(time.time, time.sleep)
-
-# function to grab station data every refresh_t seconds
-def grab_station_data(sc): 
+# grab station data every refresh_t seconds
+def grab_station_data(sc, refresh_t, save_path): 
+    """
+    inputs:
+        sc
+            type: scheduler object
+        refresh_t
+            type: int or float
+            desc: refresh time in seconds
+        save_path
+            type: str
+            desc: path to save station data to
+    """
 
     # load data
     with urllib.request.urlopen("https://gbfs.bluebikes.com/gbfs/en/station_status.json") as url:
@@ -48,8 +50,21 @@ def grab_station_data(sc):
 
 # end def grab_station_data()
 
+
+# main -------------------
+
+# set refresh time in seconds
+refresh_t = 5*60 
+
+# set directory to save data
+# save_path = 'G:\\My Drive\\Personal projects\\bluebikes\\station data'
+save_path = 'D:\\Google Drive\\Personal projects\\bluebikes\\station data'
+
+# make scheduler object
+s = sched.scheduler(time.time, time.sleep)
+
 # schedule the first execution
-s.enter(0, 1, grab_station_data, (s,))
+s.enter(0, 1, grab_station_data, (s,refresh_t,save_path))
 
 # run continuously
 s.run()
